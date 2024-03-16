2a4295c67570d710513f65f43dab3acc4bdb59a5
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-03-16 21:47:16 +0000

    tx progress

--- src/components/ens.tsx
+++ src/components/ens.tsx
@@ -1,119 +1,116 @@
 import React from "react";
-import {
-    type BaseError,
-    useWriteContract,
-    useWalletClient,
-    useWaitForTransactionReceipt,
-    usePublicClient,
-} from "wagmi";
+import { useWriteContract, useWalletClient, usePublicClient } from "wagmi";
 import { ensNameWrapperABI } from "../network/ensNameWrapperABI";
 import { namehash } from "viem";
 import { addresses } from "../network/addresses";
 import { makeid } from "../utils";
-import {Button} from "@nextui-org/react";
+import { Button, Progress } from "@nextui-org/react";
 
-// biome-ignore lint/suspicious/noEmptyInterface: <explanation>
 interface Props {
-    // Define your component's props here
+  // Define your component's props here
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ENSNameWrapper: React.FC<Props> = (_props) => {
-    const { data: walletClient } = useWalletClient();
-    const publicClient = usePublicClient();
-    const { data: hash, error, isPending, writeContract } = useWriteContract();
+const ENSNameWrapper: React.FC<Props> = () => {
+  const { data: walletClient } = useWalletClient();
+  const publicClient = usePublicClient();
+  const { writeContract } = useWriteContract();
+  const [submitting, setSubmitting] = React.useState(false);
+  const [subStep, setSubStep] = React.useState(0);
 
-    const handleSubmit = () => {
-        // TODO: Implement your submit logic here
-        const key = "wid";
-        const value = "1234567";
-        //
-        const referralCode = makeid(6);
-        //
-        const parentNode = namehash("cat.eth");
-        const node = namehash(`${referralCode}.cat.eth`);
-        //
-        writeContract(
+  const handleSubmit = () => {
+    setSubmitting(true);
+    const key = "wid";
+    const value = "1234567";
+    //
+    const referralCode = makeid(6);
+    //
+    const parentNode = namehash("cat.eth");
+    const node = namehash(`${referralCode}.cat.eth`);
+    //
+    writeContract(
+      {
+        abi: ensNameWrapperABI,
+        address: addresses.ensResolverWrapper,
+        functionName: "setSubnodeRecord",
+        args: [
+          addresses.nameWrapper,
+          parentNode,
+          referralCode,
+          addresses.ensNameOwnerAddress,
+          addresses.ensResolver,
+          0n,
+          0,
+          0n,
+        ],
+        account: walletClient!.account,
+      },
+      {
+        onSettled: async (data) => {
+          await publicClient!.waitForTransactionReceipt({
+            hash: data!,
+          });
+          setSubStep(1);
+
+          writeContract(
             {
-                abi: ensNameWrapperABI,
-                address: addresses.ensResolverWrapper,
-                functionName: "setSubnodeRecord",
-                args: [
-                    addresses.nameWrapper,
-                    parentNode,
-                    referralCode,
-                    addresses.ensNameOwnerAddress,
-                    addresses.ensResolver,
-                    0n,
-                    0,
-                    0n,
-                ],
-                // biome-ignore lint/style/noNonNullAssertion: <explanation>
-                account: walletClient!.account,
+              abi: ensNameWrapperABI,
+              address: addresses.ensResolverWrapper,
+              functionName: "setText",
+              args: [addresses.ensResolver, node, key, value],
+              account: walletClient!.account,
             },
             {
-                onSettled: async (data) => {
-                    console.log("onSettled", data);
-
-                    // NOTE: this takes a long time to resolve
-                    const tx = await publicClient!.waitForTransactionReceipt({
-                        hash: data!,
-                    });
-
-                    console.log("tx", tx);
-
-                    writeContract(
-                        {
-                            abi: ensNameWrapperABI,
-                            address: addresses.ensResolverWrapper,
-                            functionName: "setText",
-                            args: [addresses.ensResolver, node, key, value],
-                            // biome-ignore lint/style/noNonNullAssertion: <explanation>
-                            account: walletClient!.account,
-                        },
-                        {
-                            onSettled: (data) => {
-                                console.log("onSettled", data);
-                            },
-                            onSuccess: (data) => {
-                                console.log("onSuccess", data);
-                            },
-                            onError: (error) => {
-                                console.log("onError", error);
-                            },
-                        }
-                    );
-                },
-                onSuccess: (data) => {
-                    console.log("onSuccess", data);
-                },
-                onError: (error) => {
-                    console.log("onError", error);
-                },
+              onSettled: async (data) => {
+                await publicClient!.waitForTransactionReceipt({
+                  hash: data!,
+                });
+                setSubmitting(false);
+                setSubStep(0);
+              },
+              onError: () => {
+                setSubmitting(false);
+                setSubStep(0);
+              },
             }
-        );
-    };
-
-    const { isLoading: isConfirming, isSuccess: isConfirmed } =
-        useWaitForTransactionReceipt({
-            hash,
-        });
-
-    return (
-        <div style={{ margin: '3%', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-            {/* Render your component's content here */}
-            <Button color="primary" disabled={isPending} onClick={handleSubmit}>
-                Submit
-            </Button>
-
-            {hash && <div>Transaction Hash: {hash}</div>}
-            {isConfirming && <div>Waiting for confirmation...</div>}
-            {isConfirmed && <div>Transaction confirmed.</div>}
-            {error && (
-                <div>Error: {(error as BaseError).shortMessage || error.message}</div>
-            )}
-        </div>
+          );
+        },
+        onError: () => {
+          setSubmitting(false);
+          setSubStep(0);
+        },
+      }
     );
+  };
+
+  return (
+    <div
+      style={{
+        margin: "3%",
+      }}
+    >
+      <Button
+        color="primary"
+        isLoading={submitting}
+        disabled={submitting}
+        onClick={handleSubmit}
+      >
+        Submit
+      </Button>
+      {submitting && (
+        <>
+          <br />
+          <br />
+          <Progress
+            isStriped
+            aria-label="Loading..."
+            color="secondary"
+            value={subStep * 50}
+            className="max-w-md"
+          />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ENSNameWrapper;
