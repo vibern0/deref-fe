53ea3eddcd15736c71e8db70b7710425235ee7a3
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-03-16 21:02:07 +0000

    add nextui + draft header

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
-import { useAccount, useDisconnect } from 'wagmi'
-import { useWeb3Modal } from '@web3modal/wagmi/react'
-import ENSNameWrapper from './components/ens'
+import { useAccount, useDisconnect } from "wagmi";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import ENSNameWrapper from "./components/ens";
 import { WorldIdWidget } from "./components/WorldIDWidget";
+import { Tabs, Tab } from "@nextui-org/react";
+import { useState } from "react";
 
 function App() {
-	const account = useAccount()
-	const { open } = useWeb3Modal()
-	const { disconnect } = useDisconnect()
+  const account = useAccount();
+  const { open } = useWeb3Modal();
+  const { disconnect } = useDisconnect();
+  const [selected, setSelected] = useState("Home");
 
   if (account.status === "connected") {
     return (
       <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
+        <div
+          style={{
+            height: 150,
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            backgroundColor: "#ffe8d6",
+          }}
+        >
+          <h2>DeRef</h2>
+          <Tabs variant="underlined" aria-label="Tabs variants" 
+        selectedKey={selected}
+        onSelectionChange={(k) => setSelected(k.toString())}>
+            <Tab key="Home" title="Home" />
+            <Tab key="Profile" title="Profile" />
+          </Tabs>
         </div>
-
-        {account.status === "connected" && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-
-				<ENSNameWrapper />
+        {selected === "Home" ? <ENSNameWrapper /> : <>
         <WorldIdWidget signal="hoge" />
-			</div>
-		);
+              <button onClick={() => disconnect()}>Disconnect</button></>}
+      </div>
+    );
   }
-	return <button onClick={() => open()}>Connect</button>
+  return <button onClick={() => open()}>Connect</button>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,21 @@ import { WagmiProvider } from "wagmi";
 import { config } from "./network/wagmi.ts";
 import { Buffer } from "buffer";
 import App from "./App.tsx";
+import { NextUIProvider } from "@nextui-org/react";
+import "./index.css";
 
 globalThis.Buffer = Buffer;
 
 const queryClient = new QueryClient();
-
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
       </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>
